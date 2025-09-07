df6cd71b16eb9edcb52e31e18cd1880ffa1d2ca4
roman_gorokhovik <romagorohovik@gmail.com>
2025-09-07 11:36:35 +0700

    Правки по замечаниям ревьюера: добавлен тест на успешный поиск, изменен параметр фикстуры browser_settings на scope='session', исправлен TEARDOWN на 'browser.quit()', исправлен нейминг тестов, исправлены локаторы для тестов.

--- conftest.py
+++ conftest.py
@@ -2,10 +2,10 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def browser_settings():
     browser.config.window_width = 1440
     browser.config.window_height = 720
     yield
-    browser.close()
+    browser.quit()
 
--- test_hw_3.py
+++ test_hw_3.py
@@ -1,7 +1,13 @@
 from selene import browser, be, have
 
 
-def test_hw_3(browser_settings):
+def test_search_not_found(browser_settings):
     browser.open('https://duckduckgo.com/')
     browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('#######').press_enter()
-    browser.element('[class="THG_yNtlhifBrJDatoUn wZ4JdaHxSAhGy1HoNVja wN0KrcRQzChXFKiMHpCZ"]').should(have.text('ничего не найдено.'))
+    browser.element('[data-area="mainline"]').should(have.text('ничего не найдено.'))
+
+
+def test_search_found(browser_settings):
+    browser.open('https://duckduckgo.com/')
+    browser.element('[placeholder="Поиск без отслеживания"]').should(be.blank).type('qa_guru').press_enter()
+    browser.element('.//span[contains(text(), "QAGuru Inc - Quality Accelerated")]').should(have.text('QAGuru'))
